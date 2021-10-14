db8049927c88f6edc5d53c0621b9fb77a3f9a199
Thamara Andrade <tkcandrade@gmail.com>
2021-10-14 05:12:44 -0300

    Also inserting style in page

--- background.js
+++ background.js
@@ -2,6 +2,7 @@
 
 chrome.webNavigation.onHistoryStateUpdated.addListener(function (details) {
     if (details.url.match(/https:\/\/boardgamearena.com\/gamepanel\?game=*/)) {
+        chrome.tabs.insertCSS(null, { file: "styles.css" });
         chrome.tabs.executeScript(null, { file: "content.js" });
     }
 });
